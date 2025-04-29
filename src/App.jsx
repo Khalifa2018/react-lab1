81895cc3290ecb9f6d9efd8056cbceee135d479a
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-04-29 21:44:12 +0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Movies from "./components/Movies";
 
 function App() {
   return (
     <>
       <h1>Movies App</h1>
-
       <Movies />
+      <Footer/>
     </>
   );
 }
