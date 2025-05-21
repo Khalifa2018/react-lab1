54ceb705a4a77191456383d914a4d5596e5ed066
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-05-21 11:38:38 +0300

    Remove home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MoviesPage from "./pages/MoviesPage";
 import MovieDetailPage from "./pages/MovieDetailsPage";
-import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 
 function App() {
@@ -10,14 +9,12 @@ function App() {
     <>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<MoviesPage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movie/:id" element={<MovieDetailPage />} />
       </Routes>
     </>
--- src/pages/AboutPage.jsx
+++
@@ -1,9 +0,0 @@
-function AboutPage() {
-    return ( 
-        <div>
-            <h1>Welcome to Movie app About page.</h1>
-        </div>
-     );
-}
-
-export default AboutPage;
