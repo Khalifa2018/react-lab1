3aa641e11c59b1f4ff7073a2988f0aae2c6c2fa7
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-05-21 10:33:25 +0300

    Add routing

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,73 +1,65 @@
-import { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Movie extends Component {
-  render() {
-    const {
-      title,
-      overview,
-      release_date,
-      adult,
-      backdrop_path,
-      genre_ids,
-      original_language,
-      popularity,
-      poster_path,
-      video,
-      vote_average,
-      vote_count,
-    } = this.props;
+const Movie = ({
+  id,
+  title,
+  overview,
+  release_date,
+  adult,
+  backdrop_path,
+  genre_ids,
+  original_language,
+  popularity,
+  poster_path,
+  video,
+  vote_average,
+  vote_count,
+}) => {
+  const imgBase = "https://image.tmdb.org/t/p/w500";
+  const posterUrl = poster_path ? `${imgBase}${poster_path}` : null;
+  const backdropUrl = backdrop_path ? `${imgBase}${backdrop_path}` : null;
 
-    const imgBase = "https://image.tmdb.org/t/p/w500";
-    const posterUrl = poster_path ? `${imgBase}${poster_path}` : null;
-    const backdropUrl = backdrop_path ? `${imgBase}${backdrop_path}` : null;
-
-    return (
-      <div
-        className="movie-card"
-        style={
-          backdropUrl ? { backgroundImage: `url(${backdropUrl})` } : undefined
-        }
-      >
-        {posterUrl && (
-          <img
-            className="movie-poster"
-            src={posterUrl}
-            alt={`${title} poster`}
-          />
-        )}
-        <div className="movie-info">
-          <h1 className="movie-title">{title}</h1>
-          <p>
-            <strong>Release Date:</strong> {release_date}
-          </p>
-          <p>
-            <strong>Adult:</strong> {adult ? "Yes" : "No"}
-          </p>
-          <p>
-            <strong>Language:</strong> {original_language.toUpperCase()}
-          </p>
-          <p>
-            <strong>Genres:</strong> {genre_ids.join(", ")}
-          </p>
-          <p>
-            <strong>Popularity:</strong> {popularity}
-          </p>
-          <p>
-            <strong>Overview:</strong> {overview}
-          </p>
-          <p>
-            <strong>Video:</strong> {video ? "Yes" : "No"}
-          </p>
-          <p>
-            <strong>Rating:</strong> {vote_average} ({vote_count} votes)
-          </p>
-        </div>
-
-        <Link to="movie/950387">Details</Link>
+  return (
+    <div
+      className="movie-card"
+      style={
+        backdropUrl ? { backgroundImage: `url(${backdropUrl})` } : undefined
+      }
+    >
+      {posterUrl && (
+        <img className="movie-poster" src={posterUrl} alt={`${title} poster`} />
+      )}
+      <div className="movie-info">
+        <h1 className="movie-title">{title}</h1>
+        <p>
+          <strong>Release Date:</strong> {release_date}
+        </p>
+        <p>
+          <strong>Adult:</strong> {adult ? "Yes" : "No"}
+        </p>
+        <p>
+          <strong>Language:</strong> {original_language.toUpperCase()}
+        </p>
+        <p>
+          <strong>Genres:</strong> {genre_ids.join(", ")}
+        </p>
+        <p>
+          <strong>Popularity:</strong> {popularity}
+        </p>
+        <p>
+          <strong>Overview:</strong> {overview}
+        </p>
+        <p>
+          <strong>Video:</strong> {video ? "Yes" : "No"}
+        </p>
+        <p>
+          <strong>Rating:</strong> {vote_average} ({vote_count} votes)
+        </p>
       </div>
-    );
-  }
-}
+
+      <Link to={`movie/${id}`}>Details</Link>
+    </div>
+  );
+};
 
 export default Movie;
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,7 +1,28 @@
-import Movies from "../components/Movies";
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
 
 function MoviesPage() {
-  return <Movies />;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.themoviedb.org/3";
+    const path = "/discover/movie?sort_by=popularity.desc";
+    const apiKey = "&api_key=9813ce01a72ca1bd2ae25f091898b1c7";
+
+    const apiUrl = url + path + apiKey;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results));
+  }, []);
+
+  return (
+    <>
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie} />
+      ))}
+    </>
+  );
 }
 
 export default MoviesPage;
