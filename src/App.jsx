ffd8c372ba3cd03ad3ac8ccff3d4f9818dd8895c
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-05-31 15:00:15 +0300

    Add favorite count

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
+  const favorites = useSelector(state => state.favorites.movies);
+  const favoriteCount = favorites.length;
+
   return (
     <>
       <nav style={{
@@ -9,10 +13,29 @@ function App() {
         marginBottom: "2rem",
         display: "flex",
         gap: "1rem",
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
       }}>
         <Link to="/home">Home</Link>
-        <Link to="/favorites">Favorites</Link>
+        <Link to="/favorites" style={{ position: "relative" }}>
+          Favorites
+          {favoriteCount > 0 && (
+            <span style={{
+              position: "absolute",
+              top: "-8px",
+              right: "-12px",
+              backgroundColor: "#ff4444",
+              color: "white",
+              borderRadius: "50%",
+              padding: "2px 6px",
+              fontSize: "12px",
+              minWidth: "18px",
+              textAlign: "center"
+            }}>
+              {favoriteCount}
+            </span>
+          )}
+        </Link>
         <Link to="/about">About</Link>
       </nav>
       <Outlet />
